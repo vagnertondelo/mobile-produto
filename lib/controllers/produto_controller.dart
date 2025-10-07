3f82c145620ee1bf0b0e18e4721e44a2fbfdf64e
vagnertondelo <vagnertondello@gmail.com>
2025-10-07 19:28:22 -0300

    produtos listar

--- lib/controllers/produto_controller.dart
+++ lib/controllers/produto_controller.dart
@@ -1,10 +1,32 @@
+import 'package:app_produtos/models/produto_model.dart';
 import 'package:app_produtos/services/produto_service.dart';
 import 'package:get/get.dart';
 
 class ProdutoController extends GetxController{
   final ProdutoService service;
-
   ProdutoController({required this.service});
 
+  final produtos = <ProdutoModel>[].obs;
+  final isLoading = false.obs;
+  final error = ''.obs;
+
+    @override
+    void onInit(){
+      super.onInit();
+      listar();
+    }
+
+    Future<void> listar() async{
+      try{
+        isLoading.value = true;
+        error.value = '';
+        //Busca todos os produtos do serviço e atualiza a lista de forma reativa
+        produtos.assignAll(await service.listar());
+      }catch(e){
+        error.value = e.toString();
+      } finally{
+        isLoading.value = false;
+      }
+    }
 
 }
--- lib/screens/produto_list_screen.dart
+++ lib/screens/produto_list_screen.dart
@@ -1,9 +1,16 @@
+import 'package:app_produtos/controllers/produto_controller.dart';
+import 'package:app_produtos/services/produto_service.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 import '../components/menu_component.dart';
 
 class ProdutoListScreen extends StatefulWidget {
+
+  final controller = Get.put(ProdutoController(
+                          service: ProdutoService()));
+
   @override
   State<ProdutoListScreen> createState() => _ProdutoListScreenState();
 }
@@ -18,14 +25,36 @@ class _ProdutoListScreenState extends State<ProdutoListScreen> {
       ),
       drawer:
       MenuComponent(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('Tela produtos'),
-          ],
-        ),
-      ),
+      body:
+        Obx(() {
+            if(widget.controller.isLoading.value){
+              return Center(child: CircularProgressIndicator(),);
+            }
+            if(widget.controller.error.isNotEmpty){
+                return Center(child:
+                Text('Erro: ${widget.controller.error}'),);
+            }
+            if(widget.controller.produtos.isEmpty){
+              return Center(child: Text('Nenhum produto encontrado'),);
+            }
+
+            return ListView.builder(
+                itemCount: widget.controller.produtos.length,
+                itemBuilder: (variavel, i){
+                  final p = widget.controller.produtos[i];
+                  return Card(
+                    margin: EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    child: ListTile(
+                      title: Text('${p.nome} - R\$ ${p.preco.toStringAsFixed(2)}'),
+                      subtitle: Text(p.descricao),
+                      trailing: IconButton(
+                          onPressed: (){},
+                          icon: Icon(Icons.delete, color: Colors.red,)),
+                    ),
+                  );
+                }
+            );
+        })
     );
   }
 }
